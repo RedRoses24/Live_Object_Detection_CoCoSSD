18466f136e7759053d7a960bb269f0d18df3c8d2
RedRoses24 <87375785+RedRoses24@users.noreply.github.com>
2022-07-20 17:17:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+img="";
+Status="";
+objects=[];
+function setup(){
+    canvas=createCanvas(600, 500);
+    canvas.center();
+
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="CoCoSSD is detecting the object(s)";
+}
+function modelLoaded(){
+    console.log("The model is loaded!");
+    Status=true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
+}
+function preload(){
+    img=loadImage("dog_cat.jpg");
+}
+function draw(){
+image(img, 0, 0, 600, 500);
+if(Status!=""){
+    for(i=0; i<objects.length; i++){
+        document.getElementById("status").innerHTML="Status: Objects Detected";
+    fill("red");
+    stroke("red");
+    noFill();
+    percent=floor(objects[i].confidence*100);
+    text(objects[i].label+" "+percent+" %", objects[i].x+15, objects[i].y+15);
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+}
+}
